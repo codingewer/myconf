ce0e67f94de2519685e4a64b387759888a3d0eb2
codi <yatli5645@gmail.com>
2024-09-20 19:12:44 +0300

    mycnf

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+#ascii-image-converter /home/codi/wallps/codingewerlogo.png --color -m "🬄🬄" -d 50,25 |  fastfetch --file-raw -
+fastfetch
+alias ls='colorls'
+set -g fish_greeting
+end
